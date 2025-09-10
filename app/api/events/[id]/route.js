f2923af1ec7f8997b46476f5f6cad623b974ddf8
Victor <vidrums86@gmail.com>
2025-09-10 23:23:02 +0100

    fixing types error

--- app/api/events/[id]/route.js
+++ app/api/events/[id]/route.js
@@ -1,9 +1,9 @@
 import { NextResponse, NextRequest } from "next/server";
-import rawData from "@/data/events.json";
-import { type EventProps } from "@/app/components/event-list/models";
+import data from "@/data/events.json";
+// import { type EventProps } from "@/app/components/event-list/models";
 
-export async function GET(_req: NextRequest, { params }: { params: { id: string } }) {
-  const data = rawData as EventProps[];
+export async function GET(_, { params }) {
+  // const data = rawData as EventProps[];
 
   const event = data.find((e) => String(e.id) === params.id);
 
